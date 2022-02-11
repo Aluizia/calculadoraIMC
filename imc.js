dfda4011b1e5261fe5f3b81739b96dccc69b03f6
Aluizia <95940476+Aluizia@users.noreply.github.com>
2022-02-11 20:43:05 +0000

    Initial commit

---
+++ imc.js
@@ -0,0 +1,14 @@
+var nome = document.getElementById('name')
+var altura = document.getElementById('altura')
+var peso = document.getElementById('peso')
+var resultado = document.querySelector('.resultado')
+
+function calcular() {
+    if(nome.value != "" && altura.value != "" && peso.value !="") {
+        var imc = (peso.value / Math.pow(altura.value, 2)).toFixed(1)
+        resultado.innerHTML = `O IMC de ${nome.value} é: ${imc}`
+    } else {
+        resultado.innerHTML = 'Informe todos os campos!'
+    }
+}
+
